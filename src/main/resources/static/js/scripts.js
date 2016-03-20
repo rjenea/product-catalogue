d12ad3fd63bd67f3721848bd24722e4c2eebfa28
ecararus <cararuseugeniu@gmail.com>
2016-03-20 04:32:39 +0000

    Finished ui

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,6 +1,27 @@
-function products($scope, $http) {
-    $http.get('http://localhost:8080/api/catalogue/products').
-        success(function(data) {
-            $scope.products = data;
-        });
-}
+var productCatalogue = angular.module('productCatalogue',[]);
+
+productCatalogue.controller("products", function ($scope, $http, $window) {
+   $http.get('api/catalogue/products',{ withCredentials: true })
+       .success(
+           function(data) {
+                $scope.products = data;
+           }
+       )
+       .error(
+           function(data, status, headers, config) {
+                $window.location.href = 'err.html';
+           }
+       );
+
+    $scope.basket = [{}];
+
+    $scope.updateBasket = function(product) {
+		var index = $scope.basket.indexOf(product);
+    	if (index > -1) {
+        	$scope.basket.splice(index, 1);
+    	}else{
+			$scope.basket.push(product);
+    	}
+    }
+
+});
